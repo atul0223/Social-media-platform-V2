9cd29ecf3fe0f76efdbd21e1a80e417e1a9b851f
Atul_Sharma <atulbramhan@gmail.com>
2025-09-28 01:49:48 +0530

    ready

--- Frontend/src/helper/socket.ts
+++ Frontend/src/helper/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://localhost:3000", {
+const socket = io("https://social-media-platform-saas.onrender.com", {
   transports: ["websocket"],
   secure: true,
   reconnection: true,
